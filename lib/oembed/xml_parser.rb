dac3ce9ce938467f988172eee16f8ebbef00619a
Higor Morais <higorae@gmail.com>
2014-12-12 22:01:11 -0300

    Useless private access modifier.

--- lib/oembed/xml_parser.rb
+++ lib/oembed/xml_parser.rb
@@ -4,21 +4,23 @@ require 'rexml/document'
 
 module Oembed
   class XmlParser
-    def self.parse(source)
-      begin
-        document = REXML::Document.new(source)
-        traverse(document.root)
-      rescue REXML::ParseException => e
-        raise Oembed::ParserError.new(e), 'XML parser error'
+    class << self
+      def self.parse(source)
+        begin
+          document = REXML::Document.new(source)
+          traverse(document.root)
+        rescue REXML::ParseException => e
+          raise Oembed::ParserError.new(e), 'XML parser error'
+        end
       end
-    end
 
-    private
+      private
 
-    def self.traverse(node)
-      node.elements.to_a.inject({}) do |accum, elem|
-        accum[elem.name] = elem.has_elements? ? traverse(elem) : elem.text
-        accum
+      def self.traverse(node)
+        node.elements.to_a.inject({}) do |accum, elem|
+          accum[elem.name] = elem.has_elements? ? traverse(elem) : elem.text
+          accum
+        end
       end
     end
   end
