1d74bb755363d371932f4c9d554ac51466bc3d7e
Higor Morais <higorae@gmail.com>
2014-12-12 22:03:11 -0300

    Redundant begin block detected

--- lib/oembed/xml_parser.rb
+++ lib/oembed/xml_parser.rb
@@ -5,18 +5,16 @@ require 'rexml/document'
 module Oembed
   class XmlParser
     class << self
-      def self.parse(source)
-        begin
-          document = REXML::Document.new(source)
-          traverse(document.root)
-        rescue REXML::ParseException => e
-          raise Oembed::ParserError.new(e), 'XML parser error'
-        end
+      def parse(source)
+        document = REXML::Document.new(source)
+        traverse(document.root)
+      rescue REXML::ParseException => e
+        raise Oembed::ParserError.new(e), 'XML parser error'
       end
 
       private
 
-      def self.traverse(node)
+      def traverse(node)
         node.elements.to_a.inject({}) do |accum, elem|
           accum[elem.name] = elem.has_elements? ? traverse(elem) : elem.text
           accum
